39a582e5d199af9193b9886bdfde194a49762325
AMaczynski <artur.m1717@gmail.com>
2019-06-11 21:53:22 +0200

    +ScheduleService

---
+++ core/src/main/java/pl/edu/agh/service/ScheduleService.java
@@ -0,0 +1,51 @@
+package pl.edu.agh.service;
+
+import pl.edu.agh.api.IScheduleService;
+import pl.edu.agh.datamodel.Schedule;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Stateless
+@Remote(IScheduleService.class)
+public class ScheduleService extends BaseService implements IScheduleService {
+
+    @Override
+    public void addSchedules(List<Schedule> scheduleList) {
+        EntityManager em = getEntityManager();
+        for (Schedule schedule : scheduleList) {
+            em.persist(schedule);
+            em.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public List<Schedule> getSchedulesByUser(long userId) {
+        EntityManager em = getEntityManager();
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<Schedule> query = builder.createQuery(Schedule.class);
+        Root<Schedule> root = query.from(Schedule.class);
+        Predicate predicate = builder.equal(root.get("user"), userId);
+        query.where(predicate);
+        List<Schedule> schedules = em.createQuery(query).getResultList();
+        return schedules.stream()
+                .sorted(Comparator.comparing(e -> e.getDayOfWeek().toString()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void deleteSchedule(long scheduleId) {
+        EntityManager em = getEntityManager();
+        Schedule schedule = em.find(Schedule.class, scheduleId);
+        em.remove(schedule);
+        em.getTransaction().commit();
+    }
+}
